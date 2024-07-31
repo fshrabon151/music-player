e5bee9d6193a8ca1c8b42a10cd6b67344b6eaa5b
F Shrabon <fshrabon151@gmail.com>
2024-07-31 11:37:36 +0600

    added recommended video api

--- src/api/youtube.ts
+++ src/api/youtube.ts
@@ -6,23 +6,14 @@ const BASE_URL = "https://www.googleapis.com/youtube/v3";
 export const searchVideos = async (query: string | undefined) => {
   const url = `${BASE_URL}/${query ? "search" : "videos"}`;
   const response = await axios.get(url, {
-    params: query
-      ? {
-          part: "snippet",
-          maxResults: 27,
-          q: query,
-          type: "video",
-          videoCategoryId: "10", // Category ID for music
-          key: API_KEY,
-        }
-      : {
-          part: "snippet",
-          chart: "mostPopular",
-          videoCategoryId: "10", // Category ID for music
-          regionCode: "BD", // Specify your region if needed
-          maxResults: 27,
-          key: API_KEY,
-        },
+    params: {
+      part: "snippet",
+      maxResults: 27,
+      type: "video",
+      videoCategoryId: "10", // Category ID for music
+      key: API_KEY,
+      ...(query ? { q: query } : { chart: "mostPopular", regionCode: "BD" }), // Specify your region if needed
+    },
   });
   return response.data.items;
 };
@@ -38,5 +29,20 @@ export const getTrendingMusicVideos = async () => {
       key: API_KEY,
     },
   });
+  return response.data.items;
+};
+
+export const getRecommendedVideos = async (videoId: string) => {
+  const response = await axios.get(`${BASE_URL}/search`, {
+    params: {
+      part: "snippet",
+      relatedToVideoId: videoId,
+      type: "video",
+      maxResults: 10,
+      key: API_KEY,
+      videoCategoryId: "10", // Category ID for music
+    },
+  });
+
   return response.data.items;
 };
